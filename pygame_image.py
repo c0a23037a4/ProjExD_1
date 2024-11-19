c557aae788ea4ff656ec011546e50fca046a19f4
Yu Kajikura <c0a23037a4@edu.teu.ac.jp>
2024-11-19 16:41:28 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,27 @@ def main():
     flip_bg_img = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load(("fig/3.png"))
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()
     tmr = 0
+    kk_rct.center = 300, 200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         X = tmr%3200
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
         screen.blit(bg_img, [-X, 0])
         screen.blit(flip_bg_img, [-X+1600, 0])
         screen.blit(bg_img, [-X+3200, 0])
         screen.blit(flip_bg_img, [-X+4800, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(2000)
